55aedc65b74ee46ceafd003bde0ec207889f557a
anu shree <anu shree@ANUSHREE>
2019-07-31 11:22:43 +0530

    Framework code pushed

---
+++ Framework/src/test/java/com/learnautomation/testcases/MozillaTest.java
@@ -0,0 +1,18 @@
+package com.learnautomation.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MozillaTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Program Files\\Testing Software\\Mozilla Driver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://easy.techmahindra.com/ ");
+		String s = driver.getTitle();
+		System.out.println(s);
+		driver.quit();
+	}
+
+}
